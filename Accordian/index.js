f3164b7d9145f2e7084c52384d3aad97af085290
Megha-Bathla <bathlamegha6@gmail.com>
2024-11-16 13:27:04 +0530

    update

--- Accordian/index.js
+++ Accordian/index.js
@@ -1,9 +1,10 @@
-const accordian = document.querySelector("accordian");
-const title = document.querySelector("title");
-const content = document.querySelector("content");
+const accordians = document.querySelectorAll(".accordian");
+const title = document.querySelector(".title");
+const content = document.querySelector(".content");
 
-for (let i = 0; i < accordian.length; i++) {
-  accordian[i].addEventListener("click", () => {
+for (let i = 0; i < accordians.length; i++) {
+  accordians[i].addEventListener("click", function () {
     this.classList.toggle("active");
+    console.log(accordians[i]);
   });
 }
